ecb4d09e870c1ec2ca7314c4116fd37c6bf9555e
amesty <915810803@qq.com>
2015-06-25 23:27:25 +0800

    Initial Commit

---
+++ download/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  download
+//
+//  Created by amesty on 15/6/25.
+//  Copyright (c) 2015年 cf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
